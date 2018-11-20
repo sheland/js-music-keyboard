af2fa1e181e0333563495f8839d92e89d1c7623c
Shelan <shelandebesai@gmail.com>
2018-11-20 09:07:32 -0800

    complete

--- noise.js
+++ noise.js
@@ -6,35 +6,32 @@ const matchSet = {
   "note g": "gAudio",
   "note a": "aAudio",
   "note b": "bAudio",
-},
+}
 
 const keyboardSet = {
-  "cAudio": 67,
-  "dAudio": 68,
-  "eAudio": 69,
-  "fAudio": 70,
-  "gAudio": 71,
-  "aAudio": 65,
-  "bAudio": 66,
+   67: "cAudio",
+   68: "dAudio",
+   69: "eAudio",
+   70: "fAudio",
+   71: "gAudio",
+   65: "aAudio",
+   66: "bAudio",
 }
-$(document).ready( function(instrument) {   //waits for HTMLdoc to be loaded by browser then runs function
-  instrumentClass = document.getElementsByClassName(instrument.className);
 
-  for (let i = 0; i < matchSet.length; i++) {
-    if (Object.values[instrumentClass] === instrumentClass) {
-      let music = Object.values[instrumentClass],
-    } else {
-        return false
-    }
-  }
 
-  return music; //music id
+$(document).ready( function() {   //waits for HTMLdoc to be loaded by browser then runs function
+  $('.note').click(function(e) {
+      const noteClass = e.target.className; //"note a"
+      const audioId = matchSet[noteClass] // "aAudio"
+      const audio = document.getElementById(audioId); // audioElement with matching ID
+      audio.play();
+  });
 
-  let keyboardNumber = keyboardSet.Objects.values[music];
-
-  if (document.getElementsByClassName("instrumentClass").click()) || event.keyCode === keyboardNumber
-  // if (event.target.matches("instrumentClass"))
-    audio = document.getElementById("music")
-    audio.play()
-  }  
+  $('body').keydown((e) => {
+    const keyCode = e.keyCode; //71
+    const audioId = keyboardSet[keyCode]; //"gAudio"
+    const audio = document.getElementById(audioId); // audioElement with matching ID
+    debugger
+    audio.play();
+  });
 });
